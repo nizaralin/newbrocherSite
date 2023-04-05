2fe5223ace1c9404bd557b7bd17fdd69cad25b04
Nisar <114514585+nizaralin@users.noreply.github.com>
2023-04-06 00:06:16 +0530

    Arrange

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 5,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
 (function ($) {
     "use strict";
 
